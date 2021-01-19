c318bacf6808157e11fc9694409145306a3505ad
Roman Panchenko <timberspirit@gmail.com>
2021-01-19 15:58:54 +0200

    update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store'
-
+import { HashRouter } from 'react-router-dom'
 ReactDOM.render(
   <Provider store={store}>
     <App />
